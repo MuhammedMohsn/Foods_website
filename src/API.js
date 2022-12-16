653384db8df5e9f9a07a1dbd7ad010e4ce940864
muhammed mohsen <muhammedmohsen1111@gmail.com>
2022-12-16 15:36:59 +0200

    final

--- src/API.js
+++ src/API.js
@@ -1,5 +1,5 @@
 export let URL="https://api.spoonacular.com/recipes/random?number=30"
-export let apiKey="8aa402475c054e46abe798c1424fa0c9"
+export let apiKey="a508bf29f0c146e5808d81ce21902c46"
 let Get_food_data=async(URL,apiKey,type)=>{
     let res=await fetch(`${URL}&apiKey=${apiKey}&tags=${type}`)
     let data=await res.json();
