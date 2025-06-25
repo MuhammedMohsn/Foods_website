f0709cf3b17f35bf2ce537cfad751da330c727cc
MuhammedMohsn <muhammedmohsen1111@gmail.com>
2025-06-25 13:32:14 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.css'
-import React, { Fragment, useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Foods from './Pages/Foods'
-import Category from './Pages/Category'
-import Contact from './Pages/Contact'
-import NavbarComponent from './Components/Navbar_Component'
-import Footer from './Components/Footer'
-import Food from './Pages/Food'
-import { productsContext } from './Context/ProductsContext'
+import "./App.css";
+import React, { Fragment, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Foods from "./Pages/Foods";
+import Category from "./Pages/Category";
+import Contact from "./Pages/Contact";
+import NavbarComponent from "./Components/Navbar_Component";
+import Footer from "./Components/Footer";
+import Food from "./Pages/Food";
+import { productsContext } from "./Context/ProductsContext";
 function App() {
   // define global states
   // the first 3 states to get data related to type of food
-  let [popularReceipes, setPopularReceipes] = useState([])
-  let [vegetarianReceipes, setVegetarianReceipes] = useState([])
-  let [dessertReceipes, setDessertReceipes] = useState([])
+  let [popularReceipes, setPopularReceipes] = useState([]);
+  let [vegetarianReceipes, setVegetarianReceipes] = useState([]);
+  let [dessertReceipes, setDessertReceipes] = useState([]);
   // type state used for filtering from popular or dessert or vegetarian
-  let [receipeType,setReceipeType]=useState("popular")
+  let [receipeType, setReceipeType] = useState("popular");
   return (
     <Fragment>
       <productsContext.Provider
@@ -33,7 +33,7 @@ function App() {
           setDessertReceipes,
         }}
       >
-        <HashRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <NavbarComponent />
           <br />
           <Routes>
@@ -45,10 +45,10 @@ function App() {
             <Route path="/contact" element={<Contact />} />
           </Routes>
           <Footer />
-        </HashRouter>
+        </BrowserRouter>
       </productsContext.Provider>
     </Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
